49c2305d6105b084c656b7a962097a6875421b47
cvabc <jinzhang.cv@gmail.com>
2020-07-01 16:05:42 +0900

    code indent

--- draw.cpp
+++ draw.cpp
@@ -18,15 +18,15 @@ void Draw::dodraw(void) {
     num_pixel = track->get_disp_data(start, ppp, canvas->width, min, max);
     int height = canvas->height;
     for (int i = 0; i < num_pixel; i++) {
-            int _min = (min[i] - 128) * vscale + height / 2;
-            int _max = (max[i] - 128) * vscale + height / 2;
-            for (int y = _min; y <= _max; y++) { // should use <= here otherwise missing points
-                int _y = height - y;
-                _y = std::min(_y, height-1);
-                _y = std::max(_y, 0);
-                canvas->set(i, _y);
-            }
-		}
+        int _min = (min[i] - 128) * vscale + height / 2;
+        int _max = (max[i] - 128) * vscale + height / 2;
+        for (int y = _min; y <= _max; y++) { // should use <= here otherwise missing points
+            int _y = height - y;
+            _y = std::min(_y, height-1);
+            _y = std::max(_y, 0);
+            canvas->set(i, _y);
+        }
+	}
 	canvas->draw();
     tb_present();
     mutex.unlock();
